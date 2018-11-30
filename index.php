696865c0b4e441b6fd83e973473e60ff0eda7525
Martin Veyrard <veyrard.m@gmail.com>
2018-11-30 10:24:33 +0100

    ajout doc php + doc css

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+    <title></title>
+  </head>
+  <body>
+
+    <h1>Températures</h1>
+
+  </body>
+</html>
