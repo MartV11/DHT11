dfa4430e074e06eb4502823ff340d52a382bf6a2
Martin Veyrard <veyrard.m@gmail.com>
2018-12-07 16:47:50 +0100

    version final

--- index.php
+++ index.php
@@ -9,9 +9,10 @@
 
     <h1>Températures</h1>
     <?php
-    $json_url = "data.txt";
-    $json=file_get_contents($json_url);
+    $filename = "data.txt";
+    $json=file_get_contents($filename);
     $data=json_decode($json);
+    $date_update = filemtime($filename);
 
 
     $bargraph_height = 161 + $data->temperature * 4;
@@ -20,7 +21,10 @@
 
      ?>
 
-     <p> il fait <?php echo $data->temperature;?>°C avec <?php echo $data->humidite ?>% d'humidité. le 30/11/2018 </p>
+     <p> il fait <?php echo $data->temperature;?>°C avec <?php echo $data->humidite ?>% d'humidité. le 30/11/2018 <br>
+       Le <?php echo date("d/m/Y", $date_update) ; ?>
+       à <?php echo date("H:i:s", $date_update) ; ?>.
+     </p>
 
      <div id="thermometer">
      <div id="bargraph" style="height:<?php echo $bargraph_height; ?>px;top:<?php echo $bargraph_top; ?>px;"></div>
